e6fb13747bfa6c852364d93ef6d567f780a4810a
Alexandru Rebenciuc <alrebenciuc@gmail.com>
2022-03-27 18:37:01 +0300

    Prepare rails app with external specs

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_26_094347) do
+ActiveRecord::Schema.define(version: 2022_03_27_151644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookmarks", force: :cascade do |t|
-    t.string "comment"
-    t.integer "movie_id"
-    t.integer "list_id"
+    t.text "comment"
+    t.bigint "movie_id", null: false
+    t.bigint "list_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["list_id"], name: "index_bookmarks_on_list_id"
+    t.index ["movie_id"], name: "index_bookmarks_on_movie_id"
   end
 
   create_table "lists", force: :cascade do |t|
@@ -36,7 +38,8 @@ ActiveRecord::Schema.define(version: 2022_03_26_094347) do
     t.string "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "list_id"
   end
 
+  add_foreign_key "bookmarks", "lists"
+  add_foreign_key "bookmarks", "movies"
 end
